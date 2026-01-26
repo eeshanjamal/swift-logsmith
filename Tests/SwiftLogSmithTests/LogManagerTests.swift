24f9a46dbff3d3752b652b066005a751d4467ca6
Eeshan Jamal <eeshan.jamal@gmail.com>
2026-01-26 15:55:57 +0530

    Removed some boilerplate code from Log manager tests using extension and optional log completion handler.

--- Tests/SwiftLogSmithTests/LogManagerTests.swift
+++ Tests/SwiftLogSmithTests/LogManagerTests.swift
@@ -14,7 +14,6 @@ final class FirstMockLogger: ILogger, @unchecked Sendable {
     var formatter: LogFormatter
     var lastLoggedMessage: LogMessage?
     var logCallCount = 0
-    var expectation: XCTestExpectation?
 
     init(tagger: LogTagger? = nil, formatter: LogFormatter = LogFormatter.default) {
         self.tagger = tagger
@@ -25,13 +24,11 @@ final class FirstMockLogger: ILogger, @unchecked Sendable {
         lastLoggedMessage = message
         logCallCount += 1
         completion?(true)
-        expectation?.fulfill()
     }
     
     func reset() {
         lastLoggedMessage = nil
         logCallCount = 0
-        expectation = nil
     }
 }
 
@@ -40,7 +37,6 @@ final class SecondMockLogger: ILogger, @unchecked Sendable {
     var formatter: LogFormatter
     var lastLoggedMessage: LogMessage?
     var logCallCount = 0
-    var expectation: XCTestExpectation?
 
     init(tagger: LogTagger? = nil, formatter: LogFormatter = LogFormatter.default) {
         self.tagger = tagger
@@ -51,13 +47,11 @@ final class SecondMockLogger: ILogger, @unchecked Sendable {
         lastLoggedMessage = message
         logCallCount += 1
         completion?(true)
-        expectation?.fulfill()
     }
     
     func reset() {
         lastLoggedMessage = nil
         logCallCount = 0
-        expectation = nil
     }
 }
 
@@ -79,13 +73,13 @@ final class LogManagerTests: XCTestCase {
     }
 
     func testInitialization_ShouldContainDefaultLogger() {
-        let expectation = XCTestExpectation(description: "Default logger should receive log")
-        mockDefaultLogger.expectation = expectation
-        
-        sut.log(message: "test", logType: .info)
-        
-        wait(for: [expectation], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1)
+        let logger = mockDefaultLogger
+        expectCompletion(description: "Default logger should receive log") { fulfill in
+            sut.log(message: "test", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1)
+                fulfill()
+            }
+        }
     }
 
     func testAddLogger_WithNewLogger_ShouldSucceed() {
@@ -97,38 +91,36 @@ final class LogManagerTests: XCTestCase {
         }
     
         // Verify both loggers receive the message
-        let logExpectation = XCTestExpectation(description: "Both loggers should receive the log")
-        logExpectation.expectedFulfillmentCount = 2
-        mockDefaultLogger.expectation = logExpectation
-        newLogger.expectation = logExpectation
-        
-        sut.log(message: "test", logType: .info)
-        
-        wait(for: [logExpectation], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1)
-        XCTAssertEqual(newLogger.logCallCount, 1)
+        let logger = mockDefaultLogger
+        expectCompletion(description: "Both loggers should receive the log") { fulfill in
+            sut.log(message: "test", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1)
+                XCTAssertEqual(newLogger.logCallCount, 1)
+                fulfill()
+            }
+        }
     }
 
     func testLogLevelFiltering_ManagerLevel() {
         sut.setMinimumLogLevel(.error)
         
-        let expectation = XCTestExpectation(description: "Info log should be filtered")
-        expectation.isInverted = true
-        mockDefaultLogger.expectation = expectation
-
+        let logger = mockDefaultLogger
         // This log is below the manager's minimum level and should be dropped
-        sut.log(message: "info log", logType: .info)
-
-        wait(for: [expectation], timeout: 1.0)
+        expectCompletion(description: "Info log should be filtered") { fulfill in
+            sut.log(message: "info log", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 0, "Info log should have been filtered")
+                fulfill()
+            }
+        }
         
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 0, "Info log should have been filtered")
         mockDefaultLogger.reset()
 
-        let expectation2 = XCTestExpectation(description: "Error log should pass")
-        mockDefaultLogger.expectation = expectation2
-        sut.log(message: "error log", logType: .error)
-        wait(for: [expectation2], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1, "Error log should have been processed")
+        expectCompletion(description: "Error log should pass") { fulfill in
+            sut.log(message: "error log", logType: .error) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1, "Error log should have been processed")
+                fulfill()
+            }
+        }
     }
 
     func testLogLevelFiltering_LoggerItemLevel() {
@@ -139,36 +131,35 @@ final class LogManagerTests: XCTestCase {
             }
         }
         
-        let logExpectation = XCTestExpectation(description: "Default logger receives, strict logger filters")
-        mockDefaultLogger.expectation = logExpectation
-        let logExpectation2 = XCTestExpectation(description: "Strict logger should not receive the log")
-        logExpectation2.isInverted = true
-        strictLogger.expectation = logExpectation2
-        
-        sut.log(message: "info log", logType: .info)
-        
-        wait(for: [logExpectation, logExpectation2], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1)
-        XCTAssertEqual(strictLogger.logCallCount, 0)
+        let logger = mockDefaultLogger
+        expectCompletion(description: "Default logger receives, strict logger filters") { fulfill in
+            sut.log(message: "info log", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1)
+                XCTAssertEqual(strictLogger.logCallCount, 0)
+                fulfill()
+            }
+        }
     }
 
     func testLogTypeFiltering_ManagerLevel() {
         sut.setMinimumLogType(.warning)
         
-        let expectation = XCTestExpectation(description: "Info log should be filtered by type")
-        expectation.isInverted = true
-        mockDefaultLogger.expectation = expectation
-        sut.log(message: "info log", logType: .info)
-        wait(for: [expectation], timeout: 1.0)
-
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 0, "Info log should have been filtered")
+        let logger = mockDefaultLogger
+        expectCompletion(description: "Info log should be filtered by type") { fulfill in
+            sut.log(message: "info log", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 0, "Info log should have been filtered")
+                fulfill()
+            }
+        }
+        
         mockDefaultLogger.reset()
 
-        let expectation2 = XCTestExpectation(description: "Warning log should pass")
-        mockDefaultLogger.expectation = expectation2
-        sut.log(message: "warning log", logType: .warning)
-        wait(for: [expectation2], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1, "Warning log should have been processed")
+        expectCompletion(description: "Warning log should pass") { fulfill in
+            sut.log(message: "warning log", logType: .warning) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1, "Warning log should have been processed")
+                fulfill()
+            }
+        }
     }
 
     func testLogTypeFiltering_LoggerItemLevel() {
@@ -179,48 +170,48 @@ final class LogManagerTests: XCTestCase {
             }
         }
         
-        let logExpectation = XCTestExpectation(description: "Default logger receives, strict logger filters by type")
-        mockDefaultLogger.expectation = logExpectation
-        let logExpectation2 = XCTestExpectation(description: "Strict logger should not receive the log")
-        logExpectation2.isInverted = true
-        strictLogger.expectation = logExpectation2
-
-        sut.log(message: "info log", logType: .info)
-        
-        wait(for: [logExpectation, logExpectation2], timeout: 1.0)
-
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1, "Default logger should have received the info log")
-        XCTAssertEqual(strictLogger.logCallCount, 0, "Strict logger should have filtered the info log by type")
+        let logger = mockDefaultLogger
+        expectCompletion(description: "Default logger receives, strict logger filters by type") { fulfill in
+            sut.log(message: "info log", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1, "Default logger should have received the info log")
+                XCTAssertEqual(strictLogger.logCallCount, 0, "Strict logger should have filtered the info log by type")
+                fulfill()
+            }
+        }
     }
 
     func testCombinedFiltering_ManagerLevel() {
         sut.setMinimumLogLevel(.error)   // min level rawValue = 3
         sut.setMinimumLogType(.critical) // min type rawValue = 8
         
+        let logger = mockDefaultLogger
         // Scenario 1: Fails Level filter
-        let expectation1 = XCTestExpectation(description: "Log should be filtered by level")
-        expectation1.isInverted = true
-        mockDefaultLogger.expectation = expectation1
-        sut.log(message: "info log", logType: .info) // Fails level (level .info(1) < .error(3))
-        wait(for: [expectation1], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 0, "Info log should be filtered by level")
+        expectCompletion(description: "Log should be filtered by level") { fulfill in
+            sut.log(message: "info log", logType: .info) { _ in
+                XCTAssertEqual(logger?.logCallCount, 0, "Info log should have been filtered by level")
+                fulfill()
+            }
+        }
+        
         mockDefaultLogger.reset()
         
         // Scenario 2: Fails Type filter
-        let expectation2 = XCTestExpectation(description: "Log should be filtered by type")
-        expectation2.isInverted = true
-        mockDefaultLogger.expectation = expectation2
-        sut.log(message: "error log", logType: .error) // Passes level (.error(3) >= .error(3)), Fails type (.error(6) < .critical(8))
-        wait(for: [expectation2], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 0, "Error log should be filtered by type")
+        expectCompletion(description: "Log should be filtered by type") { fulfill in
+            sut.log(message: "error log", logType: .error) { _ in
+                XCTAssertEqual(logger?.logCallCount, 0, "Error log should be filtered by type")
+                fulfill()
+            }
+        }
+        
         mockDefaultLogger.reset()
         
         // Scenario 3: Passes both filters
-        let expectation3 = XCTestExpectation(description: "Log should pass both filters")
-        mockDefaultLogger.expectation = expectation3
-        sut.log(message: "critical log", logType: .critical) // Passes both (level .fault(4) >= .error(3) AND type .critical(8) >= .critical(8))
-        wait(for: [expectation3], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1, "Critical log should pass both filters")
+        expectCompletion(description: "Log should pass both filters") { fulfill in
+            sut.log(message: "critical log", logType: .critical) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1, "Critical log should pass both filters")
+                fulfill()
+            }
+        }
     }
     
     func testCombinedFiltering_LoggerItemLevel() {
@@ -234,17 +225,14 @@ final class LogManagerTests: XCTestCase {
             }
         }
         
-        let logExpectation = XCTestExpectation(description: "Combined filter test")
-        mockDefaultLogger.expectation = logExpectation
-        let strictExpectation = XCTestExpectation(description: "Strict logger should not receive the log")
-        strictExpectation.isInverted = true
-        strictLogger.expectation = strictExpectation
-        
-        sut.log(message: "warning log", logType: .warning) // Level is .error, Type is .warning. Should pass default but fail strict.
-        
-        wait(for: [logExpectation, strictExpectation], timeout: 1.0)
-        XCTAssertEqual(mockDefaultLogger.logCallCount, 1)
-        XCTAssertEqual(strictLogger.logCallCount, 0)
+        let logger = mockDefaultLogger
+        expectCompletion(description: "Combined filter test") { fulfill in
+            sut.log(message: "warning log", logType: .warning) { _ in
+                XCTAssertEqual(logger?.logCallCount, 1)
+                XCTAssertEqual(strictLogger.logCallCount, 0)
+                fulfill()
+            }
+        }
     }
 
     func testTagIntegration_ManagerAndLoggerLevels() {
@@ -262,23 +250,20 @@ final class LogManagerTests: XCTestCase {
             loggerTagger.addTag(loggerTag) { _ in fulfill() }
         }
 
-        let defaultLoggerExpectation = XCTestExpectation(description: "Default logger should receive log")
-        let specificLoggerExpectation = XCTestExpectation(description: "Specific logger should receive log")
-        mockDefaultLogger.expectation = defaultLoggerExpectation
-        loggerSpecificLogger.expectation = specificLoggerExpectation
-        
-        sut.log(message: "tag test", logType: .info)
-        
-        wait(for: [defaultLoggerExpectation, specificLoggerExpectation], timeout: 1.0)
-        
-        let defaultMsg = mockDefaultLogger.lastLoggedMessage
-        XCTAssertNotNil(defaultMsg)
-        XCTAssertTrue(defaultMsg?.tags.contains(where: { $0.identifier == "ManagerTag" }) ?? false)
-        XCTAssertFalse(defaultMsg?.tags.contains(where: { $0.identifier == "LoggerTag" }) ?? false)
-        
-        let specificMsg = loggerSpecificLogger.lastLoggedMessage
-        XCTAssertNotNil(specificMsg)
-        XCTAssertTrue(specificMsg?.tags.contains(where: { $0.identifier == "ManagerTag" }) ?? false)
-        XCTAssertTrue(specificMsg?.tags.contains(where: { $0.identifier == "LoggerTag" }) ?? false)
+        let logger = mockDefaultLogger
+        expectCompletion(description: "All loggers should receive log with correct tags") { fulfill in
+            sut.log(message: "tag test", logType: .info) { _ in
+                let defaultMsg = logger?.lastLoggedMessage
+                XCTAssertNotNil(defaultMsg)
+                XCTAssertTrue(defaultMsg?.tags.contains(where: { $0.identifier == "ManagerTag" }) ?? false)
+                XCTAssertFalse(defaultMsg?.tags.contains(where: { $0.identifier == "LoggerTag" }) ?? false)
+                
+                let specificMsg = loggerSpecificLogger.lastLoggedMessage
+                XCTAssertNotNil(specificMsg)
+                XCTAssertTrue(specificMsg?.tags.contains(where: { $0.identifier == "ManagerTag" }) ?? false)
+                XCTAssertTrue(specificMsg?.tags.contains(where: { $0.identifier == "LoggerTag" }) ?? false)
+                fulfill()
+            }
+        }
     }
 }
